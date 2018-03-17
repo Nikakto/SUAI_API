0ef0eaf0c1ede9905cc8b32c7131d7af68e840be
Nikakto <black-z@bk.ru>
2018-03-17 17:08:26 +0300

    Fix top-down. Add path

--- Lab_1/TransformationMatrix.py
+++ Lab_1/TransformationMatrix.py
@@ -15,6 +15,28 @@ class TransformationMatrix(UserList):
         DNA_1 = ' ' + DNA_1
         self.DNA = [DNA_0, DNA_1]
 
+    def __str__(self):
+
+        empty_space = [' ' * 5]
+
+        header = ['%-5s' % f'{char}' for char in self.DNA[0]]
+        string_rows = [''.join(empty_space + header)]
+
+        for row_index, row in enumerate(self):
+
+            string_row = ['%-5s' % f'{self.DNA[1][row_index]}']
+            for column in row:
+
+                if column >= 0:
+                    value = ' %-4s' % f'{column}'
+                else:
+                    value = '%-5s' % f'{column}'
+                string_row.append(value)
+
+            string_rows.append(''.join(string_row))
+
+        return '\n'.join(string_rows)
+
     def _diff(self, row, column, similarity_matrix=None):
 
         if similarity_matrix:
@@ -23,6 +45,56 @@ class TransformationMatrix(UserList):
             return similarity_matrix[nucleotide_row][nucleotide_column]
         else:
             return self.DNA[0][column] != self.DNA[1][row]
+
+    def _get_minimum(self, row, column, maximum):
+
+        can_insert = column - 1 >= 0
+        can_remove = row - 1 >= 0
+
+        if not can_insert and not can_remove:
+            return None
+
+        cells = []
+        if can_insert:
+            cells.append((row, column - 1))
+
+        if can_remove:
+            cells.append((row - 1, column))
+
+        if can_insert and can_remove:
+            cells.append((row - 1, column - 1))
+
+        if maximum:
+            return max(cells, key=lambda cell: (self[cell[0]][cell[1]], cell[0], cell[1]))
+        else:
+            return min(cells, key=lambda cell: (self[cell[0]][cell[1]], cell[0], cell[1]))
+
+    def _get_minimum_price(self, row, column, gap=1, similarity_matrix=None):
+
+        can_insert = column - 1 >= 0
+        can_remove = row - 1 >= 0
+
+        if not can_insert and not can_remove:
+            return 0
+
+        values = []
+
+        if can_insert:
+            values.append(self[row][column - 1] + gap)
+
+        if can_remove:
+            values.append(self[row - 1][column] + gap)
+
+        if can_insert and can_remove:
+            diff = self._diff(row, column, similarity_matrix)
+            values.append(self[row - 1][column - 1] + diff)
+
+        if similarity_matrix:
+            # look max similarity
+            return max(values)
+        else:
+            # look minimum operation cost
+            return min(values)
 
     def balance_bottom_up(self, row=None, column=None, gap=1, similarity_matrix=None):
 
@@ -55,7 +127,7 @@ class TransformationMatrix(UserList):
             else:
 
                 insert = self.balance_bottom_up(row, column - 1, gap, similarity_matrix) + gap
-                delete = self.balance_bottom_up(row -1, column, gap, similarity_matrix) + gap
+                delete = self.balance_bottom_up(row - 1, column, gap, similarity_matrix) + gap
 
                 diff = self._diff(row, column, similarity_matrix)
                 substitute = self.balance_bottom_up(row - 1, column - 1, gap, similarity_matrix) + diff
@@ -69,50 +141,39 @@ class TransformationMatrix(UserList):
 
         return self[row][column]
 
-    def balance_top_down(self, row=None, column=None, gap=1, similarity_matrix=None):
+    def balance_top_down(self, gap=1, similarity_matrix=None):
 
-        self.operations += 1
+        self.clear()
+        self.operations = 0
 
-        if column is None and row is None:
+        for i in range(len(self.DNA[1])):
+            self.append([float('inf')] * len(self.DNA[0]))
 
-            self.clear()
-            self.operations = 0
+        row = 0
+        column = 0
+        self[row][column] = 0
 
-            for i in range(len(self.DNA[1])):
-                self.append([float('inf')] * len(self.DNA[0]))
+        def get_diagonal(row_d, column_d):
+            result = []
+            while row_d < len(self.DNA[1]) and column_d >= 0:
+                result.append((row_d, column_d))
+                row_d += 1
+                column_d -= 1
 
-            # if similarity_matrix is not None:
-            #
-            #     for column in range(len(self.DNA[0])):
-            #         self[0][column] = gap * column
-            #
-            #     for row in range(len(self.DNA[1])):
-            #         self[row][0] = gap * row
+            return result
 
-            row = 0
-            column = 0
-            self[row][column] = 0
+        row, column = 0, 0
+        while column < len(self.DNA[0]) and row < len(self.DNA[1]):
 
-        # print(self)
-        can_insert = column+1 < len(self[row])
-        can_remove = row+1 < len(self)
-        insert = self[row][column] + gap
-        delete = self[row][column] + gap
+            cells = get_diagonal(row, column)
+            for cell_row, cell_column in cells:
+                self.operations += 1
+                self[cell_row][cell_column] = self._get_minimum_price(cell_row, cell_column, gap, similarity_matrix)
 
-        if can_insert and can_remove:
-            diff = self._diff(row + 1, column + 1, similarity_matrix)
-            substitute = self[row][column] + diff
-            if self[row + 1][column + 1] == float('inf') or self[row + 1][column + 1] < substitute:
-                self[row + 1][column + 1] = substitute
-                self.balance_top_down(row + 1, column + 1, gap, similarity_matrix)
-
-        if can_remove and (self[row + 1][column] == float('inf') or self[row + 1][column] < delete):
-            self[row + 1][column] = delete
-            self.balance_top_down(row + 1, column, gap, similarity_matrix)
-
-        if can_insert and (self[row][column + 1] == float('inf') or self[row][column + 1] < insert):
-            self[row][column + 1] = insert
-            self.balance_top_down(row, column + 1, gap, similarity_matrix)
+            if column < len(self.DNA[0]) - 1:
+                column += 1
+            else:
+                row += 1
 
     def generate_similarity_matrix(self):
 
@@ -126,24 +187,39 @@ class TransformationMatrix(UserList):
 
         return similarity_matrix
 
-    def __str__(self):
+    def get_aligment(self, maximum=False):
 
-        empty_space = [' ' * 5]
+        path = self.get_path(maximum=maximum)
+        aligment = ''
+        for i in range(len(path)-1):
+            cell_from, cell_to = path[i], path[i+1]
 
-        header = ['%-5s' % f'{char}' for char in self.DNA[0]]
-        string_rows = [''.join(empty_space + header)]
+            if cell_from[0] == cell_to[0]:
+                aligment += 'I'
+            elif cell_from[1] == cell_to[1]:
+                aligment += 'D'
+            elif self.DNA[1][cell_from[0]] == self.DNA[0][cell_from[1]]:
+                aligment += 'M'
+            else:
+                aligment += 'R'
 
-        for row_index, row in enumerate(self):
+        return ''.join(reversed(aligment))
 
-            string_row = ['%-5s' % f'{self.DNA[1][row_index]}']
-            for column in row:
+    def get_path(self, row=None, column=None, maximum=False):
 
-                if column >= 0:
-                    value = ' %-4s' % f'{column}'
-                else:
-                    value = '%-5s' % f'{column}'
-                string_row.append(value)
+        row = len(self.DNA[1]) - 1 if row is None else row
+        column = len(self.DNA[0]) - 1 if column is None else column
 
-            string_rows.append(''.join(string_row))
+        previous = self._get_minimum(row, column, maximum)
+        if previous is None:
+            return [(row, column)]
+        else:
+            return [(row, column)] + self.get_path(previous[0], previous[1], maximum)
 
-        return '\n'.join(string_rows)
+    def print_aligment(self, maximum=False):
+
+        print(f'FROM: {self.DNA[1]}')
+        print(f'TO  : {self.DNA[0]}')
+        print(f'{"="*20}')
+
+        aligment = self.get_aligment(maximum=maximum)
--- Lab_1/__init__.py
+++ Lab_1/__init__.py
@@ -1,5 +1,5 @@
+import matplotlib.pyplot as plt
 import numpy as np
-
 
 from Lab_1.TransformationMatrix import TransformationMatrix
 from Lab_1.const import NUCLEOTIDES, SIMILARITY_MATRIX
@@ -7,9 +7,11 @@ from Lab_1.const import NUCLEOTIDES, SIMILARITY_MATRIX
 
 if __name__ == '__main__':
 
-    n, m = 10, 10
-    DNA_0 = np.random.choice(NUCLEOTIDES, n)
-    DNA_1 = np.random.choice(NUCLEOTIDES, m)
+    DNA_0 = 'GAGGTAGGGAAC'
+    DNA_1 = 'TGACCCCACG'
+
+    # DNA_0 = ''.join(np.random.choice(NUCLEOTIDES, 12))
+    # DNA_1 = ''.join(np.random.choice(NUCLEOTIDES, 10))
 
     # debug
     DNA_0 = 'EDITING'
@@ -24,17 +26,48 @@ if __name__ == '__main__':
     # DNA_1 = 'GATTACA'
 
     transformation = TransformationMatrix(DNA_0, DNA_1)
+
     # transformation.balance_bottom_up()
     # print('\n', transformation)
     # print('\nOperations [Recursive]: ', transformation.operations, end='\n\n')
 
-    transformation.balance_bottom_up(gap=-1, similarity_matrix=transformation.generate_similarity_matrix())
-    # transformation.balance_recursive(gap=-1, similarity_matrix=SIMILARITY_MATRIX)
-    print('\n', transformation)
-    print('\nOperations [bottom-up]: ', transformation.operations, end='\n\n')
+    # transformation.balance_bottom_up(gap=-1, similarity_matrix=SIMILARITY_MATRIX)
+    # transformation.balance_bottom_up(gap=-1, similarity_matrix=transformation.generate_similarity_matrix())
+    # print('PATH: ', transformation.get_path(maximum=True))
+    # transformation.balance_bottom_up()
+    # print('PATH: ', transformation.get_path())
+    # print('\n', transformation)
+    # print('\nOp/erations [bottom-up]: ', transformation.operations, end='\n\n')
 
-    transformation.balance_top_down(gap=-1, similarity_matrix=transformation.generate_similarity_matrix())
-    # transformation.balance_recursive(gap=-1, similarity_matrix=SIMILARITY_MATRIX)
+    # transformation.balance_top_down(gap=-1, similarity_matrix=SIMILARITY_MATRIX)
+    # transformation.balance_top_down(gap=-1, similarity_matrix=transformation.generate_similarity_matrix())
+    # print('PATH: ', transformation.get_path(maximum=True))
+    # print('ALIGMENT:', transformation.get_aligment(maximum=True))
+    transformation.balance_top_down()
+    print('PATH: ', transformation.get_path())
+    print('ALIGMENT:', transformation.get_aligment())
     print('\n', transformation)
     print('\nOperations [top-down]: ', transformation.operations, end='\n\n')
     print('Done')
+
+    recurs = [0]
+    linear = [0]
+    for n in range(1, 25):
+
+        DNA_0 = ''.join(np.random.choice(NUCLEOTIDES, n))
+        DNA_1 = ''.join(np.random.choice(NUCLEOTIDES, n))
+
+        transformation = TransformationMatrix(DNA_0, DNA_1)
+
+        transformation.balance_bottom_up()
+        recurs.append(transformation.operations)
+
+        transformation.balance_top_down()
+        linear.append(transformation.operations)
+
+    line_top_down, = plt.plot(range(25), linear, 'k-', label='top_down')
+    line_bottom_up, = plt.plot(range(25), recurs, 'k--', label='bottom_up')
+    plt.legend(handles=[line_top_down, line_bottom_up])
+    plt.xlabel('n, n=m')
+    plt.ylabel('operations')
+    plt.show()
